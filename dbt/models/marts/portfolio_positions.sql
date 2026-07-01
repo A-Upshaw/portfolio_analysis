148ce597aa09246809cf76964e4a1d27734e4c74
Anthony Upshaw <boobiebuild@gmail.com>
2026-06-30 21:41:22 -0400

    Added weight_avg_cost column

--- dbt/models/marts/portfolio_positions.sql
+++ dbt/models/marts/portfolio_positions.sql
@@ -29,7 +29,8 @@ select
     round(p.shares * p.purchase_price,2) as cost_basis,
     round(p.shares * cp.current_price,2) as current_value,
     round((p.shares * cp.current_price) - (p.shares * p.purchase_price), 2) as unrealized_gain_loss,
-    round((cp.current_price - p.purchase_price)/ p.purchase_price * 100,2) as unrealized_gain_loss_pct
+    round((cp.current_price - p.purchase_price)/ p.purchase_price * 100,2) as unrealized_gain_loss_pct,
+    round(sum(shares * purchase_price) OVER (PARTITION BY p.ticker, p.account) / sum(shares) OVER (PARTITION BY p.ticker, p.account),4) as weighted_avg_cost
 from {{ ref('stg_portfolio') }} p
 left join current_prices cp on p.ticker = cp.ticker
 left join stocks s on p.ticker = s.ticker
